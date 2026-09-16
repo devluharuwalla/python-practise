b1d5f7253c80063c229769b1eac6fe8ee6da5ca7
Dev Luharuwalla <devluharuwalla22@gmail.com>
2026-09-16 19:22:23 -0400

    basic recap

---
+++ if.py
@@ -0,0 +1,20 @@
+age = int(input("enter your age :"))
+
+if age>= 18:
+    print("you are signed up")
+elif age< 0:
+    print("you are not born")
+else:
+    print("you r underage")
+
+
+response = input('would you like fish?')
+
+if response == 'Y':
+    print("have food")
+else:print("no food")
+a= 8
+b=6
+
+max-num = a if a<b else b
+
---
+++ string.py
@@ -0,0 +1,13 @@
+name = input("enter your full naem: ")
+
+#result=len(name)
+# find is for first occurance
+
+#result = name.find('b') 
+
+result = name.rfind('h')
+
+result = name.capatilize()
+result = name.upper()
+
+print(result)
